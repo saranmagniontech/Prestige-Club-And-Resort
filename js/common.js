f213bbfaff941296aec89e5df6f8372327e3f4c1
saravanan9168 <saravanan9168@mountzion.ac.in>
2025-09-19 22:14:46 +0530

    Track large files with Git LFS

--- js/common.js
+++ js/common.js
@@ -1,26 +1,3 @@
-//aos
-AOS.init({
-    once: true
-});
-
-// scroll top - docxify
-let mybutton = document.getElementById("topbtn");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById('topbtn').style.display='block';
-    } else {
-        document.getElementById('topbtn').style.display='none';
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-$(document).ready(function() {
-	setTimeout(function(){
-		$('body').addClass('loaded');
-	}, 500);
-
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:00c27801c2664cc870cd73f9f82e87673e4c726b113dc313b91bd6c22d35da7d
+size 627
